e592018f253926ffae1876b30c00d9ae5b48a984
Greg Morgan <gregm9999@gmail.com>
2018-11-25 21:36:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,8 +24,8 @@ class Application
     elsif req.path.match(/add/)
       search_term=req.params["item"]
       if @@items.include?(search_term)
-        @@cart << item
-        resp.write "added #{item}"
+        @@cart << search_term
+        resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
       end
