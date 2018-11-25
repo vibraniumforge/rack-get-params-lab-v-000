c54421878f7a7258a584caebf823682034294f74
Greg Morgan <gregm9999@gmail.com>
2018-11-25 21:33:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
       end
     elsif req.path.match(/add/)
       if @@items.include?("item")
-        @@cart << @@items[item]
+        @@cart << item
         resp.write "Added #{item}"
       else
         resp.write "We don't have that item"
