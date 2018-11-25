1b5a6228dbcc875c1d5c5733153472f1fc09bbbb
Greg Morgan <gregm9999@gmail.com>
2018-11-25 21:28:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      if @@cart.size==0
+        resp.write "Your cart is empty"
       resp.write "#{@@cart}"
     elsif req.path.match(/add/)
       if @@items.indlude?("item")
