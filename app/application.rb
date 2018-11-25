15be41237cbe62708823a59950c918542eb3fdde
Greg Morgan <gregm9999@gmail.com>
2018-11-25 21:25:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,9 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart)
-      @@cart
+      resp.write "#{@@cart}"
+    elsif req.path.match(/add)
+
     else
       resp.write "Path Not Found"
     end
